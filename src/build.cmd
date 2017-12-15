c1c9681514a9b51b48849cbaa992709ab0a19d07
Kozelek Petr <petr.kozelek@gmail.com>
2017-12-15 20:56:23 +0100

    Fix build process in VS 2017.

--- src/build.cmd
+++ src/build.cmd
@@ -7,4 +7,4 @@ set Configuration=Release
 set PROJ_FILE=%ROOT_DIR%build.proj
 set /p PackageVersion="Insert version of a packake to be created (i.e. 1.5.4): "
 
-MSBuild.exe "%PROJ_FILE%" /t:CreatePackage /p:PackageVersion=%PackageVersion% /p:Configuration=%Configuration%
+"d:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" "%PROJ_FILE%" /t:CreatePackage /p:PackageVersion=%PackageVersion% /p:Configuration=%Configuration%
